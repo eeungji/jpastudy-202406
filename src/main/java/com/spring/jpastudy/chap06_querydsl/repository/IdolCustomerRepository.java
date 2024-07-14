ebf12baa14cb145fc584d4c88a666acfb68084f0
kimeunji <eunji93121@gmail.com>
2024-07-14 22:31:47 +0900

    페이징 정렬 연습문제 / Study

--- src/main/java/com/spring/jpastudy/chap06_querydsl/repository/IdolCustomerRepository.java
+++ src/main/java/com/spring/jpastudy/chap06_querydsl/repository/IdolCustomerRepository.java
@@ -1,15 +1,20 @@
 package com.spring.jpastudy.chap06_querydsl.repository;
 
 import com.spring.jpastudy.chap06_querydsl.entity.Idol;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface IdolCustomerRepository {
 
+    // JPA의 PAGE인터페이스를 사용
+    Page<Idol> foundAllByPageing(Pageable pageable);
+
     //이름으로 오름차해서 전체조회
-    List<Idol> findAllSortedByName();
+    List<Idol> foundAllName2();
 
     //그룹명으로 아이돌을 조회
-    List<Idol> findByGroupName();
+    List<Idol> foundByGroupName();
 
 }
--- src/main/java/com/spring/jpastudy/chap06_querydsl/repository/IdolRepositoryImpl.java
+++ src/main/java/com/spring/jpastudy/chap06_querydsl/repository/IdolRepositoryImpl.java
@@ -3,21 +3,50 @@ package com.spring.jpastudy.chap06_querydsl.repository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.spring.jpastudy.chap06_querydsl.entity.Idol;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
+
 import static com.spring.jpastudy.chap06_querydsl.entity.QIdol.*;
 
 @Repository
 @RequiredArgsConstructor
-public class IdolRepositoryCustomerImpl implements IdolCustomerRepository{
+//customerRepository 만들 때 규칙이 접두사로 jpa를 구현한 인터페이스 이름
+//마지막에 impl이라고 꼭 써야함
+public class IdolRepositoryImpl implements IdolCustomerRepository{
 
     private final JdbcTemplate template;
 
     private final JPAQueryFactory factory; //쿼리DSL 사용하고 싶을 때 주입받기
 
     @Override
-    public List<Idol> findAllSortedByName() {
+    public Page<Idol> foundAllByPageing(Pageable pageable) {
+
+        // 페이징을 통한 조회
+        List<Idol> idolList = factory
+                .selectFrom(idol)
+                .orderBy(idol.age.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        // 총 조회건수
+        Long totalCount = Optional.ofNullable(
+                factory
+                        .select(idol.count())
+                        .from(idol)
+                        .fetchOne()
+        ).orElse(0L);
+
+        return new PageImpl<>(idolList, pageable, totalCount);
+    }
+
+    @Override
+    public List<Idol> foundAllName2() {
         String sql = "SELECT * FROM tbl_idol ORDER BY idol_name ASC";
         return template.query(sql, (rs, n) -> {
 
@@ -34,7 +63,7 @@ public class IdolRepositoryCustomerImpl implements IdolCustomerRepository{
 
     //서비스랑 쿼리DSL 연결할 때 사용하는 방법
     @Override
-    public List<Idol> findByGroupName() {
+    public List<Idol> foundByGroupName() {
         return factory
                 .select(idol)
                 .from(idol)
